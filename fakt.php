f3c4298aa9ee97e2b59d256b1f50accb38662704
mixmasteru <ulrichpech@gmail.com>
2015-12-23 15:37:29 +0100

    fix 404 handling

--- fakt.php
+++ fakt.php
@@ -11,23 +11,30 @@ $fakt = "";
 $min = 8801;
 $max = 17700;
 $try = 0;
+$url = "";
 
 while($try++ <= 5)
 {
 	$rand= rand($min,$max);
 	$url = "http://unnuetzes-wissen.neon.de/entry/".$rand;
 	$headers = get_headers($url);
-	if($headers[0] == "HTTP/1.1 404 Not Found")
+
+	if(strpos($headers[0],"404 Not Found") !== false) {
 		continue;
+	}
 
 	$tags = get_meta_tags($url);
 	$fakt = $tags["description"];
+	break;
 }
 
 $arr =  array(	"message" => $fakt,
-		"color" => 'green',
-		"message_format" => "text"		
-	);
+				"color" => 'green',
+				"message_format" => "text",
+				"url" => $url
+		);
 
 header("content-type: application/json",200);
-echo json_encode($arr); 
+echo json_encode($arr);
+
+
